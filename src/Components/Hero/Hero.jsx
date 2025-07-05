0dd168bb24f9cdb6bee415d86f911cab05a1a8b6
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-07-05 16:20:40 +0530

    hero component completed

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,25 +1,59 @@
 import "./Hero.css";
 import userIcon from "../../assets/user.svg";
 import CartIcon from "../../assets/cart.svg";
+import Couch from "../../assets/couch.png";
+import greenDots from '../../assets/dots-green.svg'
 
 const Hero = () => {
   return (
     <div className="hero-container">
+      {/* navbar */}
+
       <div className="nav-container">
         <div className="logo">
           <h1>Furni.</h1>
         </div>
         <div className="menu">
-          <a href="#" className="active"><span></span>Home</a>
-          <a href="#"><span></span>Shop</a>
-          <a href="#"><span></span>About Us</a>
-          <a href="#"><span></span>Services</a>
-          <a href="#"><span></span>Blog</a>
-          <a href="#"><span></span>Contact us</a>
+          <a href="#" className="active">
+            <span></span>Home
+          </a>
+          <a href="#">
+            <span></span>Shop
+          </a>
+          <a href="#">
+            <span></span>About Us
+          </a>
+          <a href="#">
+            <span></span>Services
+          </a>
+          <a href="#">
+            <span></span>Blog
+          </a>
+          <a href="#">
+            <span></span>Contact us
+          </a>
         </div>
         <div className="icons">
-          <img src={userIcon} alt="" className="icon1"/>
+          <img src={userIcon} alt="" className="icon1" />
           <img src={CartIcon} alt="" />
+        </div>
+      </div>
+
+      {/* hero section */}
+
+      <div className="main-container">
+        <div className="text-container">
+          <h1>Modern Interior Design Studio</h1>
+          <p>
+            Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet
+            velit. Aliquam vulputate velit imperdiet dolor tempor tristique
+          </p>
+          <button>Shop Now</button>
+          <button>Explore</button>
+        </div>
+        <div className="image-container">
+      <img src={Couch} alt="" className="sofa-image" />
+      <img src={greenDots} alt="" className="green-dots" />
         </div>
       </div>
     </div>
